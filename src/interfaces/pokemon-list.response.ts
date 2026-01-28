51b8be1569b53c80eb543782afe321954bf93b73
vmontesgarcia <victormontesgarcia.87@gmail.com>
2026-01-28 16:23:15 -0600

    Actualiza

--- src/interfaces/pokemon-list.response.ts
+++ src/interfaces/pokemon-list.response.ts
@@ -1,11 +1,11 @@
 export interface PokemonListResponse {
-    count:    number;
-    next:     string;
-    previous: null;
-    results:  Result[];
+  count: number;
+  next: string;
+  previous: null;
+  results: Result[];
 }
 
 export interface Result {
-    name: string;
-    url:  string;
+  name: string;
+  url: string;
 }
