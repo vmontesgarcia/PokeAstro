1f6f8a71cd1de9917c093339632d7cbbf8c2af0b
vmontesgarcia <victormontesgarcia.87@gmail.com>
2026-01-28 15:35:59 -0600

    Deploy Astro to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+site: 'https://vmontesgarcia.github.io',
+  base: '/PokeAstro/',
+  output: 'static',
+    vite: {
+    plugins: [tailwindcss()],
+  },
+});
